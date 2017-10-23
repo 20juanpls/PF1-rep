9cf2ae1bfbb983b485aa94123ec0d9bcd199510e
20juanpls <shortwavepeak11@gmail.com>
2017-10-23 12:17:42 -0500

    updated program6

--- Program6/program_6.py
+++ Program6/program_6.py
@@ -31,7 +31,6 @@ while P_cont:
 
 print('\nRequirement 5')
 print('processing sales data...')
-print('\nRequirement 6 & 7')
 sales_data = []
 for week in weeks:
     weekly_records = []
@@ -45,48 +44,9 @@ for week in weeks:
         weekly_records.append(daily_records)
     sales_data.append(weekly_records)
 
-# print(str(sales_data))
+print(str(sales_data))
 
-print('\nRequirement 8')
-print("Type in the compliments given by the respective user for the following, "
-      "If user has no further compliment's, press ENTER without typing")
-print('\nRequirement 9')
-complements_data = []
-for person in sales_reps.keys():
-    print('    '+person+"'s compliments:")
-    FinalNumOfComp = 0
-    Comp_c = True
-    count = 0
-    while Comp_c:
-        Comp_int = input('   '+str(count+1)+'. ')
-        if Comp_int == '':
-            FinalNumOfComp = count
-            Comp_c = False
-        count += 1
-    complements_data.append(FinalNumOfComp)
-print(str(complements_data))
 
-print('\nRequirement 10')
-PersonCount = 0
-for person, dealership in sales_reps.items():
-    print('--------------------------------------')
-    print(person+' of '+dealership)
-    print('Cars Sold')
-    totalCars = 0
-    WeekCount = 0
-    for week in weeks:
-        print('\n'+week)
-        DayCount = 0
-        for day in days:
-            CarsSold = sales_data[WeekCount][DayCount][PersonCount]
-            print(day+': ' + str(CarsSold))
-            totalCars += CarsSold
-            DayCount += 1
-        WeekCount += 1
-    print("\nTotal Cars Sold (per rep): " + str(totalCars))
-    print("Average Cars Sold (per rep): " + str(round(totalCars / 6)))
-    print("Number of Compliments (per rep): " + complements_data[PersonCount])
-    PersonCount += 1
 
 
 
